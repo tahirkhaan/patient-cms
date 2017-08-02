7eb5322c888a6612113e6cd548c28f27ef509d75
peerfahad <fahadrashid611@gmail.com>
2017-08-02 18:32:08 +0500

    files for login

---
+++ login_patient.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <meta charset="UTF-8">
+  <title>Sign Up Form with live validation</title>
+  
+  
+  
+      <link rel="stylesheet" href="css/style.css">
+
+  
+</head>
+
+<body>
+  	
+
+<form action="#" method="post">
+  <h2>Patient Login</h2>
+		<p>
+			<label for="patientid" class="floatLabel">Patient ID</label>
+			<input id="Email" name="Email" type="text">
+		</p>
+		<p>
+			<label for="password" class="floatLabel">Password</label>
+			<input id="password" name="password" type="password">
+			<span>Enter a password longer than 8 characters</span>
+		</p>
+		<p>
+			<input type="submit" value="Login" id="submit">
+		</p>
+	</form>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+</html>
