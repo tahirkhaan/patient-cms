d390a735c6800e8df95e752589b28b4a8fcbebb5
peerfahad <fahadrashid611@gmail.com>
2017-08-03 01:15:14 +0500

    Doctor logged in file

---
+++ doctor-logged-in.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Doctor Logged in</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container main">
+        <h1 id="doctor">Doctor Logged In</h1>
+        <hr>
+        <center>
+            <p id="patient">List of Patients</p>
+            <table class="table table-hover table-bordered" cellspacing="0" width="100%">
+                <thead class="thead-inverse">
+                    <tr>
+                        <th>Patient id</th>
+                        <th>Patient Name</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><a href="">001</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                    <tr>
+                        <td><a href="">002</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                    <tr>
+                        <td><a href="">003</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                    <tr>
+                        <td><a href="">004</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                    <tr>
+                        <td><a href="">005</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                    <tr>
+                        <td><a href="">006</a>
+                        </td>
+                        <td>tahir</td>
+                    </tr>
+                </tbody>
+            </table>
+        </center>
+    </div>
+
+    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+
+    <script src="js/index.js"></script>
+
+</body>
+
+</html>
