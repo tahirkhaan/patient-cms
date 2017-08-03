620131f4a3d0598a2c706f17e2fce8594584e81f
peerfahad <fahadrashid611@gmail.com>
2017-08-03 23:16:03 +0500

    creating session for patient

--- login/session_login.php
+++ login/session_login.php
@@ -1,21 +1,17 @@
 <?php
 session_start();
+include ("config/conn.php");
 
-include("config/conn.php");
-
-
-if (isset($_SESSION['username'])) {
-    $user_check = $_SESSION['username'];
-    
-    $ses_sql = mysqli_query($conn, "select * from users where username = '$user_check' ");
-    
-    $row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
-    
-    $login_session = $row['username'];
-    
-    
-} else {
-    header("location:login_patient.php");
+if (isset($_SESSION['username']))
+{
+	$user_check = $_SESSION['username'];
+	$ses_sql = mysqli_query($conn, "select * from users where username = '$user_check' ");
+	$row = mysqli_fetch_array($ses_sql, MYSQLI_ASSOC);
+	$login_session = $row['username'];
+}
+else
+{
+	header("location:login_patient.php");
 }
 
 ?>
