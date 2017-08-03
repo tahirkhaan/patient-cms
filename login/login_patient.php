f888560a858bac93b4a691d174ae7cc6d15aac59
peerfahad <fahadrashid611@gmail.com>
2017-08-03 23:14:51 +0500

    php script for login patient

--- login/login_patient.php
+++ login/login_patient.php
@@ -1,32 +1,28 @@
 <?php
 session_start();
-include("../config/conn.php");
+include ("../config/conn.php");
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // username and password sent from form 
-    
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+    // username and password sent from form
     $myusername = mysqli_real_escape_string($conn, $_POST['patientname']);
     $mypassword = mysqli_real_escape_string($conn, $_POST['password']);
-    
-    $sql    = "SELECT * FROM `users` WHERE username = '$myusername' and password = '$mypassword'";
+    $sql = "SELECT * FROM `users` WHERE username = '$myusername' and password = '$mypassword'";
     $result = mysqli_query($conn, $sql);
-    $row    = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $active = $row['active'];
-    
     $count = mysqli_num_rows($result);
-    
+
     // If result matched $myusername and $mypassword, table row must be 1 row
-    
-    if ($count == 1) {
+    if ($count == 1)
+    {
         $_SESSION['username'] = $myusername;
-        
-        
         header("location: ../patient_logged_in.php");
-    } else {
-        $error = "Your Login Name or Password is invalid";
+    }
+    else
+    {
         echo "<script> alert('Invalid username or password!') </script>";
-        
-        
     }
 }
+
 ?>
