631c96e40dbc7cb80284f10dd9b44f205846255e
peerfahad <fahadrashid611@gmail.com>
2017-08-03 23:17:18 +0500

    patient login page

--- login_patient.php
+++ login_patient.php
@@ -1,23 +1,20 @@
 <?php include "header.php"; ?>
-<body>  	
-<div class="box">
-<form method="POST" action="login/login_patient.php">
-  <h2>Patient Login</h2>
-  
-		<p>
-			<label for="patientname" class="floatLabel">Patient Name</label>
-			<input id="patientname" name="patientname" type="text">
-		</p>
-		<p>
-			<label for="password" class="floatLabel">Password</label>
-			<input id="password" name="password" type="password">
-			<span>Enter a password longer than 8 characters</span>
-		</p>
-		<p>
-		<button type="submit" id="btnn" class="btn">Login</button>
-			<!-- <input type="submit" value="Login"  id="submit" > -->
-		</p>
-	</form>
-	</div>
-
-<?php include "footer.php"; ?>
+<body>
+  <div class="box">
+    <form method="POST" action="login/login_patient.php">
+      <h2>Patient Login</h2>
+      <p>
+        <label for="patientname" class="floatLabel">Patient Name</label>
+        <input id="patientname" name="patientname" type="text">
+      </p>
+      <p>
+        <label for="password" class="floatLabel">Password</label>
+        <input id="password" name="password" type="password">
+        <span>Enter a password longer than 8 characters</span>
+      </p>
+      <p>
+        <button type="submit" id="btnn" class="btn">Login</button>
+      </p>
+    </form>
+  </div>
+  <?php include "footer.php"; ?>
