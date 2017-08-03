178e0431aeb1c105ef0f84dcbaee1134a33f8df7
peerfahad <fahadrashid611@gmail.com>
2017-08-04 00:02:32 +0500

    format the code

--- login_doctor.php
+++ login_doctor.php
@@ -1,23 +1,20 @@
 <?php include "header.php"; ?>
-
 <body>
-  	
-<div class="box">
-<form action="#" method="post">
-  <h2>Doctor Login</h2>
-		<p>
-			<label for="doctorid" class="floatLabel">Doctor ID</label>
-			<input id="doctorid" name="doctorid" type="text">
-		</p>
-		<p>
-			<label for="password" class="floatLabel">Password</label>
-			<input id="password" name="password" type="password">
-			<span>Enter a password longer than 8 characters</span>
-		</p>
-		<p>
-			<input type="submit" value="Login" id="submit">
-		</p>
-	</form>
-	</div>
-
-<?php include "footer.php"; ?>
+  <div class="box">
+    <form action="login/login_doctor.php" method="post">
+      <h2>Doctor Login</h2>
+      <p>
+        <label for="doctorname" class="floatLabel">Doctor Name</label>
+        <input id="doctorname" name="doctorname" type="text">
+      </p>
+      <p>
+        <label for="password" class="floatLabel">Password</label>
+        <input id="password" name="password" type="password">
+        <span>Enter a password longer than 8 characters</span>
+      </p>
+      <p>
+        <button type="submit" id="btnn" class="btn">Login</button>
+      </p>
+    </form>
+  </div>
+ <?php include "footer.php"; ?>
