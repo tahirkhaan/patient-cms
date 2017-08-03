0b9bc84a8b6c9a97714ea4730120a65579a1abe1
Fahad Murtaza <fahdim@gmail.com>
2017-08-03 05:03:45 +0500

    Adding title to home

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 
 <head>
   <meta charset="UTF-8">
-  <title>Main Page</title>
+  <title>Patient CMS</title>
 
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
