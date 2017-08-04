a04708b245bab9cff8415201239585aa937a2c4e
peerfahad <fahadrashid611@gmail.com>
2017-08-04 14:56:45 +0500

    camelCase used and echo invalid username message

--- login/login_patient.php
+++ login/login_patient.php
@@ -5,10 +5,10 @@ include ("../config/conn.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // username and password sent from form
-    $myusername = mysqli_real_escape_string($conn, $_POST['patientname']);
-    $mypassword = mysqli_real_escape_string($conn, $_POST['password']);
+    $myUserName = mysqli_real_escape_string($conn, $_POST['patientname']);
+    $myPassword = mysqli_real_escape_string($conn, $_POST['password']);
     $type = "patient";
-    $sql = "SELECT * FROM `users` WHERE username = '$myusername' and password = '$mypassword' and type = '$type'";
+    $sql = "SELECT * FROM `users` WHERE usersname = '$myUserName' and password = '$myPassword' and type = '$type'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $active = $row['active'];
@@ -22,8 +22,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     }
     else
     {
-        echo "<script> alert('Invalid username or password!') </script>";
+        echo "invalid user name or password ";
     }
 }
+
+
 
 ?>
