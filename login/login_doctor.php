8a0523b8fb7cdf494015fe170b372f66399e5620
peerfahad <fahadrashid611@gmail.com>
2017-08-04 14:54:12 +0500

    Used camelCase

--- login/login_doctor.php
+++ login/login_doctor.php
@@ -5,10 +5,10 @@ include ("../config/conn.php");
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
     // username and password sent from form
-    $myusername = mysqli_real_escape_string($conn, $_POST['doctorname']);
-    $mypassword = mysqli_real_escape_string($conn, $_POST['password']);
+    $myUserName = mysqli_real_escape_string($conn, $_POST['doctorname']);
+    $myPassword = mysqli_real_escape_string($conn, $_POST['password']);
     $type = "doctor";
-    $sql = "SELECT * FROM `users` WHERE username = '$myusername' and password = '$mypassword' and type = '$type'";
+    $sql = "SELECT * FROM `users` WHERE usersname = '$myUserName' and password = '$myPassword' and type = '$type'";
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
     $active = $row['active'];
@@ -17,12 +17,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
     // If result matched $myusername and $mypassword, table row must be 1 row
     if ($count == 1)
     {
-        $_SESSION['username'] = $myusername;
+        $_SESSION['usersname'] = $myusername;
         header("location: ../doctor_logged_in.php");
     }
     else
     {
-        echo "<script> alert('Invalid username  password or user type') </script>";
+        echo "invalid user name or password ";
+        // function error(){
+        //  $_SESSION['error'] = "invalid user name";
+        //  $error = $_SESSION['error'] ;
+        //  // print_r($_SESSION);
+        //  return $error;
+        // } 
     }
 }
 
