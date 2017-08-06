706411571c84cc8c98917e0aa5cad4aabeda4586
peerfahad <fahadrashid611@gmail.com>
2017-08-06 23:11:58 +0500

    register interface

--- register.php
+++ register.php
@@ -7,7 +7,6 @@ if (!isset($_GET["type"])) {
 } else {
 
     $registerType = $_GET['type'];
-    // echo $registerType;
     if ($registerType === "patient") {
         $submitType = "patient_register";
         $heading = "Register Patient";
