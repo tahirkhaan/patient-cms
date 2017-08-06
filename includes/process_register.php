4669ada0db5d3355f68dfb0e2c2cd945258de4fc
peerfahad <fahadrashid611@gmail.com>
2017-08-07 01:57:06 +0500

    updated files

--- includes/process_register.php
+++ includes/process_register.php
@@ -15,8 +15,9 @@ if (isset($_POST["patient_register"]) || isset($_POST["doctor_register"])) {
     }
     if (isUnique($email)) {
         include "../config/conn.php";
-        $sql = "insert into users (usersname, password, phone, email, type)  values ('$username','$userPass','$phoneNumber','$email','$type')";
-        if (mysqli_query($conn, $sql)) {
+        $sql1 = "insert into users (name, username, password, phone, email, type)  values 
+        ('$name','$userName','$userPass','$phoneNumber','$email','$type')";
+        if (mysqli_query($conn, $sql1)) {
             header("location: ../login.php");
         }
     } else {
