d290474f082815c0be594b7685534a8b2ee7e676
Fahad Murtaza <info@fahdmurtaza.com>
2017-08-06 13:48:13 +0500

    Functionality for get variables that are supposed to go in the MySQL, debugging needed!

---
+++ input.php
@@ -0,0 +1,43 @@
+<?php
+
+include("config/conn.php");
+
+    if (!isset($_GET) || !empty($_GET) && !empty($_GET['user_id']) && !empty($_GET['pulse']) && !empty($_GET['bp1']) && !empty($_GET['bp2']) && !empty($_GET['temp']) && !empty($_GET['glucose'])) {
+
+    $userId = $_GET['user_id'];
+    $pulse = $_GET['pulse'];
+    $bp1 = $_GET['bp1'];
+    $bp2 = $_GET['bp2'];
+    $glucose = $_GET['glucose'];
+    $temperature = $_GET['temp'];
+
+    if (saveDataInDatabase($userId, $pulse, $bp1, $bp2, $glucose, $temperature)) {
+        echo "Success";
+    } else {
+        echo "Fail";
+    }
+} else {
+    echo "Please send the params i.e userid,  pulse = 'pulse' and BP as bp1 and bp2, glucose as glucose, temperature as temp";
+}
+
+function saveDataAsCsv($userId, $pulse, $bp1, $bp2, $glucose, $temperature)
+{
+    $list = array($userId, $pulse, $bp1, $bp2, $glucose, $temperature);
+    $file = fopen("data.csv", "a+");
+    if (fputcsv($file, $list)) {
+        fclose($file);
+        return true;
+    }
+    return false;
+}
+
+function saveDataInDatabase($userId, $pulse, $bp1, $bp2, $glucose, $temperature)
+{
+
+    $sql = "insert into patient_readings (user_id, pulse, bp1, bp2, glucose,temp) values ($userId, $pulse, $bp1, $bp2, $glucose, $temperature)";
+    if (mysqli_query($conn, $sql)) {
+       return true;
+    } else {
+        return false;
+    }
+}
--- mysql_dumb/patient_cms.sql
+++ mysql_dumb/patient_cms.sql
@@ -1,30 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Aug 04, 2017 at 04:24 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `patient_cms`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `patient_medication`
---
 
 CREATE TABLE `patient_medication` (
   `id` int(11) NOT NULL,
@@ -32,18 +8,9 @@ CREATE TABLE `patient_medication` (
   `medication` varchar(5000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `patient_medication`
---
 
 INSERT INTO `patient_medication` (`id`, `user_id`, `medication`) VALUES
 (1, 32, 'sfn,ba,nbdnasbd,sabd,bas');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `patient_readings`
---
 
 CREATE TABLE `patient_readings` (
   `id` int(11) UNSIGNED NOT NULL,
@@ -56,18 +23,10 @@ CREATE TABLE `patient_readings` (
   `glucose` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `patient_readings`
---
 
 INSERT INTO `patient_readings` (`id`, `user_id`, `timestamp`, `pulse`, `bp1`, `bp2`, `temp`, `glucose`) VALUES
 (1, 32, '2017-08-04 12:03:22', 12, 12, 12, 12, 12);
 
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
 
 CREATE TABLE `users` (
   `id` int(11) UNSIGNED NOT NULL,
