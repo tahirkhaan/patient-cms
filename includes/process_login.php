40e8c93431f56847c8e3d886081aa3fd02e6fc2e
peerfahad <fahadrashid611@gmail.com>
2017-08-06 16:01:16 +0500

    one login for both but have some problem after login always direct to condition one

---
+++ includes/process_login.php
@@ -0,0 +1,41 @@
+<?php
+ session_start();
+include ("../config/conn.php");
+
+if (isset($_POST["patient_login"]) || isset($_POST["doctor_login"])) {
+
+    // username and password sent from form
+    $myUserEmail = mysqli_real_escape_string($conn, $_POST['email']);
+    $myPassword = mysqli_real_escape_string($conn, $_POST['password']);
+
+      if (isset($_POST["patient_login"]) ) {
+       $type = 'patient';
+    } else {
+        $type = 'doctor';
+    }
+     $sql = "SELECT * FROM `users` WHERE email = '$myUserEmail' and password = '$myPassword' and type = '$type'";
+
+
+    // $type = "doctor";
+    // $sql = "SELECT * FROM `users` WHERE email = '$myUserEmail' and password = '$myPassword' and type = '$type'";
+
+     $result = mysqli_query($conn, $sql);
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $active = $row['active'];
+    $count = mysqli_num_rows($result);
+
+  
+    // If result matched $myusername and $mypassword, table row must be 1 row
+    if ($count == 1)
+    {
+        $_SESSION['email'] = $myUserEmail;
+        header("location: ../patient_logged_in.php");
+    }
+    else
+    {
+        echo "invalid user name or password ";
+      
+    }
+}
+
+?>
