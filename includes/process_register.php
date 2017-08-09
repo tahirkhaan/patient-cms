cdf315bbfa69417876c51e7f979dec74ba7f96bb
peerfahad <fahadrashid611@gmail.com>
2017-08-09 09:10:52 +0500

    include session messages

--- includes/process_register.php
+++ includes/process_register.php
@@ -1,5 +1,5 @@
 <?php
-session_start();
+include "../config/conn.php";
 if (isset($_POST["patient_register"]) || isset($_POST["doctor_register"])) {
     $name = $_POST['name'];
     $userName = $_POST['username'];
@@ -12,26 +12,27 @@ if (isset($_POST["patient_register"]) || isset($_POST["doctor_register"])) {
     } else {
         $type = 'doctor';
     }
-    if (isUnique($email)) {
-        include "../config/conn.php";
-        $sql1 = "insert into users (name, username, password, phone, email, type)  values ('$name','$userName','$userPass','$phoneNumber','$email','$type')";
-        if (mysqli_query($conn, $sql1)) {
-            header("location: ../login.php");
-        }
-    } else {
-        $_SESSION["errormsg"] = "Email already exist";
-        header("location: ../index.php");
-    }
-}
-function isUnique($email)
-{
-    include "../config/conn.php";
-    $sql = "SELECT * FROM users where email LIKE '$email'";
+    
+
+    $sql = "SELECT * FROM users where email ='$email'";
     // Code for counting actual rows
     $result = mysqli_query($conn, $sql);
     $count = mysqli_num_rows($result);
     if ($count > 0) {
-        return false;
-    }
-    return true;
+        $_SESSION["errormsg"] = "Email already exist";
+        header("location: ../index.php");
+    }else{
+    $sql1 = "insert into users (name, username, password, phone, email, type)  values ('$name','$userName','$userPass','$phoneNumber','$email','$type')";
+        if (mysqli_query($conn, $sql1)) {
+            $_SESSION["errormsg"] = "You have successfully registered";
+            header("location: ../index.php");
+        } else{
+                $_SESSION["errormsg"] = "Email already exist";
+            header("location: ../index.php");
+        }
+        
+    } 
+
 }
+    
+
