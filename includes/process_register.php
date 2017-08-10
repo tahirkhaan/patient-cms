e907738c378aa4ea1ec0a14d8a17daefa97623be
Fahad Murtaza <fahdim@gmail.com>
2017-08-10 18:47:41 +0500

    Register process and login page fixed for all situations

--- includes/process_register.php
+++ includes/process_register.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include "../config/conn.php";
 if (isset($_POST["patient_register"]) || isset($_POST["doctor_register"])) {
     $name = $_POST['name'];
@@ -13,12 +14,17 @@ if (isset($_POST["patient_register"]) || isset($_POST["doctor_register"])) {
     } else {
         $type = 'doctor';
     }
+
     if (isUnique($email)) {
         include "../config/conn.php";
-        $sql1 = "insert into users (name, username, password, phone, email, type)  values 
+        $sql = "insert into users (name, username, password, phone, email, type)  values 
         ('$name','$userName','$userPass','$phoneNumber','$email','$type')";
-        if (mysqli_query($conn, $sql1)) {
+        if (mysqli_query($conn, $sql)) {
             header("location: ../login.php");
+        }
+        else {
+            $_SESSION["errormsg"] = "There was a problem registering, please try again";
+            header("location: ../index.php");
         }
     } else {
         $_SESSION["errormsg"] = "Email already exist";
--- login.php
+++ login.php
@@ -1,49 +1,49 @@
 <?php
-  include ("config/conn.php");
-include "header.php";
+include("config/conn.php");
 
 if (!isset($_GET["type"])) {
-    header("Location: ./index.php");
+    $LoginType  = "patient";
+    $heading = "Patient Login";
+    $label = "Patient Email";
 } else {
 
     $LoginType = $_GET['type'];
-    // echo $registerType;
     if ($LoginType === "patient") {
         $submitType = "patient_login";
-        $heading = "Login Patient";
-         $label = "Patient Email";
+        $heading = "Patient Login";
+        $label = "Email";
     } else if ($LoginType === 'doctor') {
         $submitType = "doctor_login";
-        $heading = "Login Doctor";
-        $label = "Doctor Email";
+        $heading = "Doctor Login";
+        $label = "Email";
     } else {
         header("location:./");
     }
 }
-
+include "header.php";
 ?>
 <body>
-  <div class="box">
-    <form action="includes/process_login.php" method="post" class="LoginDp">
-      <h2><?php echo $heading ?></h2>
-      <p class = "error"> <?php
-          if (isset($_SESSION['errormsg'])) {
-              echo $_SESSION['errormsg'];
-               $_SESSION['errormsg'] = "";
-          }
-          ?></p>
-<p>
-        <label for="Email" class="floatLabel"><?php echo $label ?></label>
-        <input  name="email" type="text">
-      </p>
-      <p>
-        <label for="password" class="floatLabel">Password</label>
-        <input name="password" type="password">
-       <!--  <span>Enter a password longer than 8 characters</span> -->
-      </p>
-      <p>
-        <button name="<?php echo $submitType; ?>" id="btnn" class="btn">Login</button>
-      </p>
-    </form>
-  </div>
- <?php include "footer.php"; ?>
+<div class="box">
+  <form action="includes/process_login.php" method="post" class="LoginDp">
+    <h2><?php echo $heading ?></h2>
+    <p class="error"> <?php
+        if (isset($_SESSION['errormsg'])) {
+            echo $_SESSION['errormsg'];
+            $_SESSION['errormsg'] = "";
+        }
+        ?></p>
+    <p>
+      <label for="Email" class="floatLabel"><?php echo $label ?></label>
+      <input name="email" type="text">
+    </p>
+    <p>
+      <label for="password" class="floatLabel">Password</label>
+      <input name="password" type="password">
+      <!--  <span>Enter a password longer than 8 characters</span> -->
+    </p>
+    <p>
+      <button name="<?php echo $submitType; ?>" id="btnn" class="btn">Login</button>
+    </p>
+  </form>
+</div>
+<?php include "footer.php"; ?>
