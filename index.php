15e326fdc6d1d0db173454afaafd5012243131d1
Fahad Murtaza <fahdim@gmail.com>
2017-08-12 02:00:17 +0500

    Improved styles

--- index.php
+++ index.php
@@ -4,30 +4,33 @@ include "header.php"; ?>
 
 <body>
 <div class="container home">
+  
   <h2 style="padding-top: 30px; text-align:center;font-weight: 700;">Patient Information System</h2>
+  
   <hr>
+
   <div class="row">
     <div class="col-md-12">
         
-      <div class="alert alert-success"> <?php
-          if (isset($_SESSION['errormsg'])) {
-              echo $_SESSION['errormsg'];
-              $_SESSION['errormsg'] = "";
-          }
-          ?>
-      </div>
-      
-      <div class="buttons">
-        <a href="register.php?type=patient" class="btn btn-success btn-lg btn-block btn-huge">Register as a Patient</a>
-        <br/>
-        <a href="login.php?type=patient" class="btn btn-success btn-lg btn-block btn-huge">Login as a Patient </a>
-        <br/>
-        <a href="login.php?type=doctor" class="btn btn-success btn-lg btn-block btn-huge">Login as a Doctor</a>
-        <br/>
-      </div>
+    <?php
+    if (isset($_SESSION['errormsg'])) {
+        echo '<div class="alert alert-success">' . $_SESSION['errormsg'] . '</div>' ;
+        $_SESSION['errormsg'] = "";
+    }
+    ?>
+
+    <div class="buttons">
+      <a href="register.php?type=patient" class="btn btn-success btn-lg btn-block btn-huge">Register as a Patient</a>
+      <br/>
+      <a href="login.php?type=patient" class="btn btn-success btn-lg btn-block btn-huge">Login as a Patient </a>
+      <br/>
+      <a href="login.php?type=doctor" class="btn btn-success btn-lg btn-block btn-huge">Login as a Doctor</a>
+      <br/>
+    </div>
 
     </div>
   </div>
+
 </div>
 
 <?php include "footer.php"; ?>
