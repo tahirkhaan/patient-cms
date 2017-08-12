b0905092039557af3c6331554e6abc998df3fa4b
peerfahad <fahadrashid611@gmail.com>
2017-08-12 23:06:04 +0500

    modified files

--- doctor_viewing_patient.php
+++ doctor_viewing_patient.php
@@ -46,7 +46,7 @@ VALUES ('$medication', '$patient_id')";
   if (mysqli_query($conn, $sql))
   {
     $_SESSION['updatemsg'] =  "Record updated successfully";
-    smsFeature();
+    // smsFeature();
   }
   else
   {
@@ -98,33 +98,33 @@ mysqli_close($conn); ?>
   </div>
 </div>
 <?php
-function smsFeature(){
-  // Require the bundled autoload file - the path may need to change
-// based on where you downloaded and unzipped the SDK
+// function smsFeature(){
+//   // Require the bundled autoload file - the path may need to change
+// // based on where you downloaded and unzipped the SDK
 
-    require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
-    use Twilio\Rest\Client;
-// Use the REST API Client to make requests to the Twilio REST API
+//     require __DIR__ . '/twilio-php-master/Twilio/autoload.php';
+//     use Twilio\Rest\Client;
+// // Use the REST API Client to make requests to the Twilio REST API
  
 
-// Your Account SID and Auth Token from twilio.com/console
-$sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
-$token = 'your_auth_token';
-$client = new Client($sid, $token);
+// // Your Account SID and Auth Token from twilio.com/console
+// $sid = 'ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX';
+// $token = 'your_auth_token';
+// $client = new Client($sid, $token);
 
-// Use the client to do fun stuff like send text messages!
-$client->messages->create(
-    // the number you'd like to send the message to
-    '+923129577572',
-    array(
-        // A Twilio phone number you purchased at twilio.com/console
-        'from' => '+923129577572',
-        // the body of the text message you'd like to send
-        'body' => "Hey Jenny! Good luck on the bar exam!"
-    )
-);
+// // Use the client to do fun stuff like send text messages!
+// $client->messages->create(
+//     // the number you'd like to send the message to
+//     '+923129577572',
+//     array(
+//         // A Twilio phone number you purchased at twilio.com/console
+//         'from' => '+923129577572',
+//         // the body of the text message you'd like to send
+//         'body' => "Hey Jenny! Good luck on the bar exam!"
+//     )
+// );
 
-} 
+// } 
  ?>
 
 <?php include "footer.php"; ?>
